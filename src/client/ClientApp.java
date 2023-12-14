61525b513071014fe61adb614d687588ea7e998e
abdelrhman-mamdouh <abdelrhman99m@gmail.com>
2023-12-14 15:17:43 +0200

    make exit and min buttons and hide bar

--- src/client/ClientApp.java
+++ src/client/ClientApp.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,7 +21,7 @@ public class ClientApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("HomeFXML.fxml"));
-        
+        //stage.initStyle(StageStyle.UNDECORATED); //Hide bar of the stage
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
